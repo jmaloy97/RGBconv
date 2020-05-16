81f585bc856938e1090b517987f421ceb25549c4
J Maloy <rmaloy97@icloud.com>
2020-05-15 23:39:57 -0400

    Further changes.

--- RGBconv.cpp
+++ RGBconv.cpp
@@ -16,7 +16,7 @@
 
 using namespace std;
 
-/* code sourced from 
+/* code sourced from
 	https://stackoverflow.com/questions/2654480/writing-bmp-image-in-pure-c-c-without-other-libraries. */
 const int bpp = 3; //rgb
 const int fileHeaderSize = 14;
@@ -26,48 +26,55 @@ unsigned char *createBitmapFileHeader(int height, int width, int paddingSize);
 unsigned char *createBitmapInfoHeader(int height, int width);
 /* end section */
 
-int main() {
+int main()
+{
 	//cout << "Testing to make sure I haven't gone senile.\n";
-	
+
 	string colourVal[6];
 	string a;
-	
+
 	cout << "Please enter a valid hexadecimal colour value: ";
 	cin >> a;
 	printf("\n");
-	if (a.size() <= 5 || a.size() >= 7) {
+	if (a.size() <= 5 || a.size() >= 7)
+	{
 		cout << "FATAL ERROR: Invalid hexidecimal colour code." << endl;
 		return 0;
 	}
 
-	for (int i = 0; i < a.size(); i++) {
+	for (int i = 0; i < a.size(); i++)
+	{
 		// cout << a[i] << endl;
 		char testing_char = a[i];
-		int ascii = (int) testing_char;
-		cout << "i: " << i << " - " << testing_char << ": " << ascii << endl;
-		if ((ascii >= 97) && (ascii <= 102)) {
-			cout << "Lower-case hexadecimal character." << endl;
+		int ascii = (int)testing_char;
+		//cout << "i: " << i << " - " << testing_char << ": " << ascii << endl;
+		if ((ascii >= 97) && (ascii <= 102))
+		{
+			//cout << "Lower-case hexadecimal character." << endl;
 			testing_char = toupper(testing_char);
 			colourVal[i] = testing_char;
 		}
-		else if ((ascii >= 48) && (ascii <= 57)) {
-			cout << "You've passed me a number." << endl;
+		else if ((ascii >= 48) && (ascii <= 57))
+		{
+			//cout << "You've passed me a number." << endl;
 			colourVal[i] = testing_char;
 		}
-		else if ((ascii >= 65) && (ascii <= 70)) {
-			cout << "Uppercase hexadecimal value." << endl;
+		else if ((ascii >= 65) && (ascii <= 70))
+		{
+			//cout << "Uppercase hexadecimal value." << endl;
 			colourVal[i] = testing_char;
 		}
-		else {
+		else
+		{
 			printf("Invalid character!\n");
 			exit(0);
 		}
 	}
 
-	string r_val = colourVal[0]+colourVal[1];
-	string g_val = colourVal[2]+colourVal[3];
-	string b_val = colourVal[4]+colourVal[5];
-//	cout << r_val << endl << g_val << endl << b_val << endl;
+	string r_val = colourVal[0] + colourVal[1];
+	string g_val = colourVal[2] + colourVal[3];
+	string b_val = colourVal[4] + colourVal[5];
+	//	cout << r_val << endl << g_val << endl << b_val << endl;
 	int red, green, blue;
 	red = stoi(r_val, 0, 16);
 	green = stoi(g_val, 0, 16);
@@ -81,16 +88,16 @@ int main() {
 	int height = 500;
 	int width = 500;
 	unsigned char image[height][width][bpp];
-	char* imageFileName = "output.bmp";
+	char *imageFileName = "output.bmp";
 
 	int i, j;
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
 		{
-			image[i][j][2] = red;					///red
-			image[i][j][1] = green;					///green
-			image[i][j][0] = blue; ///blue
+			image[i][j][2] = red;	///red
+			image[i][j][1] = green; ///green
+			image[i][j][0] = blue;	///blue
 		}
 	}
 
@@ -100,7 +107,7 @@ int main() {
 	return 0;
 }
 
-/* bitmap generation code sourced from 
+/* bitmap generation code sourced from
 	https://stackoverflow.com/questions/2654480/writing-bmp-image-in-pure-c-c-without-other-libraries. */
 void generateBitmapImage(unsigned char *image, int height, int width, char *imageFileName)
 {
