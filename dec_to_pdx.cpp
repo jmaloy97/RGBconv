3f046c16a18c85f3be38d083880059a96eeeee45
J Maloy <rmaloy97@icloud.com>
2021-07-27 12:52:05 -0400

    Paradox?

---
+++ dec_to_pdx.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file: dec_to_hex.cpp
+ * @desc: Converts decimal RGB values (0-255) to values that would work in Paradox Interactive video games.
+ * @author: Hermione Maxson
+ * @date: 27 July 2021 **/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <cstring>
+
+#include "convert.hpp"
+#include "bitmap.hpp"
+#include "mainMenu.hpp"
+
+using namespace std;
+
+//stub
