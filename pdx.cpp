346a37964440ac014465946cab7adee3dee640fa
J Maloy <rmaloy97@icloud.com>
2022-03-29 11:51:38 -0400

    0.2.2 - working on the code for paradox conversion
    Working on the debug logic to add paradox conversion. Will do when I
    have more energy...
    
    Signed-off-by: J Maloy <rmaloy97@icloud.com>

--- pdx.cpp
+++ pdx.cpp
@@ -20,8 +20,51 @@
 using namespace std;
 //todo: create conditions for hex or dec conversion
 
+int pdx_quitstate; //quit state
+char pdxred[3];
+char pdxgreen[3];
+char pdxblue[3];
+int ry, by, gy = 0;
+
+int pdx_numPrompt(string prompt)
+{
+    int val;
+    do
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << prompt;
+        cin >> val;
+    } while (cin.fail());
+    return val;
+}
+
+bool pdx_inBounds(int floor, int ceiling, int value)
+{
+    return ((value >= floor) && (value <= ceiling));
+}
+
+int pdx_promptInBounds(int floor, int ceiling, string prompt)
+{
+    int value = floor;
+    do
+    {
+        value = pdx_numPrompt(prompt);
+    } while (!pdx_inBounds(floor, ceiling, value));
+    return value;
+}
+
 void paradox() 
 {
-    //stub
+    if (strlen(pdxred) == 0) {
+        float ry_conv;
+        ry = pdx_promptInBounds(0, 255, "Enter a value for red: ");
+        cout << "ry:" << ry << endl;
+        //need to calculate x/255 here
+        cout << "ry_conv:" << ry_conv << endl;
+    }
+    while (!pdx_quitstate) {
+        //null
+    }
     return;
 }
